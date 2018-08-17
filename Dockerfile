1ec6efd09db59dcdbc408fdc7bf5804ea8739b54
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2018-08-17 16:56:07 +0300

    Docker config

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# install tools to image
+FROM ruby:2.5.1
+
+RUN apt-get update -qq && apt-get install -y build-essential libpq-dev nodejs
+
+# put application code inside an image
+
+RUN mkdir /my_app
+WORKDIR /my_app
+COPY Gemfile /my_app/Gemfile
+COPY Gemfile.lock /my_app/Gemfile.lock
+RUN bundle install
+COPY . /my_app
---
+++ bin/docker-dev-server.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+wait_for_db() {
+  counter=0
+  echo "Connecting to posrgress"
+  while ! nc -z db 5432; do
+    counter=$((counter+1))
+    if [ $counter == 30 ]; then
+      echo "Error: Couldn't connect to Postgres."
+      exit 1
+    fi
+    echo "Trying to connect to Postgres at $postgres_address. Attempt $counter."
+    sleep 5
+  done
+}
+
+# Clean environment
+if [ -f ./tmp/pids/server.pid ]; then
+  echo -e "\nCleaning environment"
+  rm -f tmp/pids/server.pid
+fi
+
+# Install dependencies
+echo -e "\nChecking gems"
+bundle install --quiet
+
+# Initialize database if it's required
+if [ ! -f ./tmp/db.sem ]; then
+  echo -e "\nSetup database"
+  bundle exec rake db:setup
+  echo -e "\nRunning migrations"
+  bundle exec rake db:migrate
+  touch ./tmp/db.sem
+fi
+
+# Run!
+echo -e "\nRunning server"
+bundle exec rails server -b 0.0.0.0 -p 3001
