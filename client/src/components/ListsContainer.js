be4f35128b9af7856157e64abcc55abb8b6819c2
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2018-09-04 10:26:17 +0300

    Add creating new list

--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import List from '../List';
+import NewListForm from '../NewListForm';
 
 
 class ListsContainer extends Component {
@@ -9,7 +10,21 @@ class ListsContainer extends Component {
     this.state = {
       lists: []
     }
+    this.addNewList = this.addNewList.bind(this)
   }
+
+    addNewList(title, excerpt) {
+    axios.post( 'http://localhost:3001/api/v1/lists', { list: {title, excerpt} })
+      .then(response => {
+                          console.log(response)
+                          const lists = [ ...this.state.lists, response.data ]
+                          this.setState({lists})
+                        })
+  .catch(error => {
+      console.log(error)
+  })
+  }
+
   componentDidMount() {
     axios.get('http://localhost:3001/api/v1/lists.json')
       .then(response => {
@@ -21,8 +36,10 @@ class ListsContainer extends Component {
   render() {
     return (
       <div className="lists-container">
-        { this.state.lists.map( list => { return (<List list={list} key={list.id} />) }) }
-      </div>
+      <NewListForm onNewList={this.addNewList} />
+      { this.state.lists.map( list => { return (<List list={list} key={list.id} />) }) }
+
+  </div>
   )
   }
 }
