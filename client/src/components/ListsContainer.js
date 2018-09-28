8e97695fe52fe95c8a378145cb446f479ac6fe8d
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2018-09-28 11:38:55 +0300

    Update element

--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -1,56 +1,77 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import List from '../List';
-import NewListForm from '../NewListForm';
-
+import List from './List';
+import NewListForm from './NewListForm';
+import EditListForm from './EditListForm';
 
 class ListsContainer extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state = {
-      lists: []
-    }
+    this.state = { lists: [], editingListId: null }
     this.addNewList = this.addNewList.bind(this)
     this.removeList = this.removeList.bind(this)
+    this.editingList = this.editingList.bind(this)
+    this.editList = this.editList.bind(this)
   }
 
-    addNewList(title, excerpt) {
-    axios.post( 'http://localhost:3001/api/v1/lists', { list: {title, excerpt} })
-      .then(response => {
-                          console.log(response)
-                          const lists = [ ...this.state.lists, response.data ]
-                          this.setState({lists})
-                        })
-  .catch(error => {
-      console.log(error)
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/lists')
+    .then(response => {
+      console.log(response)
+      this.setState({ lists: response.data })
     })
+    .catch(error => console.log(error))
+  }
+
+  addNewList(title, excerpt) {
+    axios.post( 'http://localhost:3001/api/v1/lists', { list: {title, excerpt} })
+    .then(response => {
+      console.log(response)
+      const lists = [ ...this.state.lists, response.data ]
+      this.setState({lists})
+    })
+    .catch(error => console.log(error))
   }
 
   removeList(id) {
-                    axios.delete( 'http://localhost:3001/api/v1/lists/' + id )
-                    .then(response => {
-                                        const lists = this.state.lists.filter(list => list.id !== id)
-                                        this.setState({lists})
-                                      })
-                    .catch(error => console.log(error))
-                  }
-
-  componentDidMount() {
-    axios.get('http://localhost:3001/api/v1/lists.json')
-      .then(response => {
-                          console.log(response)
-                          this.setState({ lists: response.data })
-                        })
-      .catch(error => console.log(error))
+    axios.delete( 'http://localhost:3001/api/v1/lists/' + id )
+    .then(response => {
+      const lists = this.state.lists.filter( list => list.id !== id )
+      this.setState({lists})
+    })
+    .catch(error => console.log(error))
   }
+
+  editingList(id) {
+    this.setState({ editingListId: id })
+  }
+
+  editList(id, title, excerpt) {
+    axios.put( 'http://localhost:3001/api/v1/lists/' + id, { list: { title, excerpt } })
+    .then(response => {
+      console.log(response);
+      const lists = this.state.lists;
+      lists[id-1] = {id, title, excerpt}
+      this.setState(() => ({ lists, editingListId: null }))
+    })
+    .catch(error => console.log(error));
+  }
+
   render() {
     return (
       <div className="lists-container">
-      <NewListForm onNewList={this.addNewList} />
-      { this.state.lists.map( list => { return (<List list={list} key={list.id} onRemoveList={this.removeList} />) }) }
+        {this.state.lists.map( list => {
+          if ( this.state.editingListId === list.id ) {
+            return (<EditListForm list={list} key={list.id} editList={this.editList} />)
+          } else {
+            return (<List list={list} key={list.id} onRemoveList={this.removeList} editingList={this.editingList}/>)
+          }
+        })}
 
-  </div>
-  )
+        <NewListForm onNewList={this.addNewList} />
+      </div>
+    )
   }
 }
+
 export default ListsContainer;
