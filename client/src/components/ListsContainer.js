7fbf67f723096589824525f6cb371324e07f5b3a
Alexey Kudryashov <kudryashov.alexey.v@gmail.com>
2018-10-29 11:43:57 +0300

    Add Apollo

--- client/src/components/ListsContainer.js
+++ client/src/components/ListsContainer.js
@@ -3,6 +3,19 @@ import axios from 'axios';
 import List from './List';
 import NewListForm from './NewListForm';
 import EditListForm from './EditListForm';
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const FEED_QUERY = gql`
+  {
+    allLists {
+      id
+      excerpt
+      title
+      description
+    }
+  }
+`;
 
 class ListsContainer extends Component {
   constructor(props) {
@@ -26,7 +39,6 @@ class ListsContainer extends Component {
   addNewList(title, excerpt) {
     axios.post( 'http://localhost:3001/api/v1/lists', { list: {title, excerpt} })
     .then(response => {
-      console.log(response)
       const lists = [ ...this.state.lists, response.data ]
       this.setState({lists})
     })
@@ -49,7 +61,6 @@ class ListsContainer extends Component {
   editList(id, title, excerpt) {
     axios.put( 'http://localhost:3001/api/v1/lists/' + id, { list: { title, excerpt } })
     .then(response => {
-      console.log(response);
       const lists = this.state.lists;
       lists[id-1] = {id, title, excerpt}
       this.setState(() => ({ lists, editingListId: null }))
@@ -59,19 +70,28 @@ class ListsContainer extends Component {
 
   render() {
     return (
-      <div className="lists-container">
-        {this.state.lists.map( list => {
-          if ( this.state.editingListId === list.id ) {
-            return (<EditListForm list={list} key={list.id} editList={this.editList} />)
-          } else {
-            return (<List list={list} key={list.id} onRemoveList={this.removeList} editingList={this.editingList}/>)
-          }
-        })}
+      <Query query={FEED_QUERY}>
+        {({ loading, error, data }) => {
+          if (loading) return <div>Fetching</div>;
+          if (error) return <div>Error</div>;
 
-        <NewListForm onNewList={this.addNewList} />
-      </div>
-    )
-  }
+          const listsToRender = data.allLists;
+
+          return (
+            <div className="lists-container">
+              {listsToRender.map(list => {
+                if (this.state.editingListId === list.id) {
+                  return (<EditListForm list={list} key={list.id} editList={this.editList}/>)
+                } else {
+                  return (<List list={list} key={list.id} onRemoveList={this.removeList} editingList={this.editingList} />)
+                }
+              })}
+                <NewListForm onNewList={this.addNewList} />
+            </div>
+          )
+        }}
+      </Query>
+    )  }
 }
 
 export default ListsContainer;
